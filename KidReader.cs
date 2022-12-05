caaa58d2c601034b2dd74e4a7d6df38035ed9192
Joachim Nilsvik <jn@strai.no>
2022-12-05 20:57:50 +0100

    updated kid reader

--- KidReader.cs
+++ KidReader.cs
@@ -44,10 +44,16 @@ public static class KidReader
         return controllDidgit == (remainder == 0 ? 0 : 10 - remainder);
     }
 
-    private static bool ValidateKidFormat(string kid) => kid.Length is < 2 or > 25 || Regex.Match(kid, @"\w").Success;
+    public static bool ValidateKidFormat(string kid) => kid.Length is < 2 or > 25 || Regex.Match(kid, @"\w").Success;
 
-    private static string[] GetKidCandidates(string txt) =>
-        Regex.Matches(txt, @"(?<=kid(.|\n){0,30} )(?<![.,\w]|\d[  ])\d{2,25}(?![.,\w ]\d)")
-            .Select(k => k.Value).Where(k => long.Parse(k) > 0)
-            .OrderByDescending(k => k.Length).ToArray();
+    public static string[] GetKidCandidates(string txt)
+    {
+        MatchCollection a = Regex.Matches(txt, @"(?<=kid(.|\n){0,30} )(?<![.,\w]|\d[  ])\d{5,25}(?![.,\w ]\d)|\d{5,25}(?![.,\w ]\d)(?=(.|\n){0,30}kid)", RegexOptions.IgnoreCase);
+        return a.Select(k => k.Value).Where(k => ulong.Parse(k) > 0).OrderByDescending(k => k.Length).ToArray();
+    }  
+    public static string[] GetKidCandidatesLite(string txt)
+    {
+        MatchCollection a = Regex.Matches(txt, @"(?=(.|\n)*)(?<![.,\w]|\d[  ])\d{2,25}(?![.,\w ]\d)", RegexOptions.IgnoreCase);
+        return a.Select(k => k.Value).OrderByDescending(k => k.Length).ToArray();
+    }
 }
